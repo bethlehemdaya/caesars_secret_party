c15aaf2536d3d877c53772c7b441c802acd5a408
Bethlehem Daya <sisterbethlehem@gmail.com>
2025-09-12 23:06:11 -0500

    first commit

---
+++ project1_ex1/app.js
@@ -0,0 +1,10 @@
+let partyLocation = "GARDEN"; // As the message might change.
+const shiftValue = 3; // Since the shift value remains constant throughout.
+let encryptedMessage = ""; // This will change when we encode the message with the original party location.
+let decryptedMessage = ""; // This will change when we decode the encrypted message to reveal the party location.
+let isPartySafe = false; // Hopefully, this doesn't change.
+
+
+const shiftValue = 3;
+Number.isInteger(shiftValue);
+
